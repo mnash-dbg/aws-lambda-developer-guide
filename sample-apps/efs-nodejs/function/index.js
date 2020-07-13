ac3f32b375708c1f5988c731acf3e5122ddb9947
Miles <miles.nash37@gmail.com>
2020-07-14 00:29:43 +0100

    Separate function deploy for testing s3 and efs

--- sample-apps/efs-nodejs/function/index.js
+++ sample-apps/efs-nodejs/function/index.js
@@ -2,45 +2,71 @@ var fs = require('fs').promises
 const crypto = require('crypto')
 const dir = process.env.mountPath
 const s3 = new (require("aws-sdk/clients/s3"))()
+let cached = false;
+let fileName;
 
-exports.handler = async function(event) {
-  console.log("EVENT: %s", JSON.stringify(event, null, 2))
-  const filePath = dir + "/" + event.fileName
-  const fileSize = event.fileSize
-
+exports.handler = async function (event) {
+  console.log("EVENT: %s", JSON.stringify(event, null, 2));
+  fileName = event.fileName;
+  const filePath = dir + "/" + event.fileName;
+  const fileSize = event.fileSize;
   // generate file
-  const buffer = await crypto.randomBytes(fileSize)
-
-  // write operations
-  const efsWriteTimeMs = await writeFileToEFS(filePath, buffer)
-  const s3WriteTimeMs = await writeFileToS3(filePath, buffer)
+  const buffer = await crypto.randomBytes(fileSize);
+  // write operation
+  const writeTimeMs = await writeFile(filePath, buffer);
 
   // read operations
   let count = process.env.readIterations || 1
-  const efsReadTimesMs = [];
-  const s3ReadTimesMs = [];
+  const readTimesMs = [];
 
   while (count) {
-    efsReadTimesMs.push(await readFileFromEFS(filePath));
-    s3ReadTimesMs.push(await readFileFromS3(filePath));
+    readTimesMs.push(await readFile(filePath));
     --count;
   }
 
   // stat file
-  const fileStat = await fs.stat(filePath)
-  const fileSizeBytes = fileStat.size
-  console.log("File size: %s bytes", fileSizeBytes)
-
+  const fileSizeBytes = await getFileSize(filePath);
+  console.log("File size: %s bytes", fileSizeBytes);
   // format response
   var response = {
-    efsWriteTimeMs,
-    s3WriteTimeMs,
-    avEfsReadTimeMs: mean(efsReadTimesMs),
-    avS3ReadTimeMs: mean(s3ReadTimesMs),
+    writeTimeMs: writeTimeMs,
+    avReadTimeMs: mean(readTimesMs),
     fileSizeBytes: fileSizeBytes,
   };
-  return response
+  return response;
 }
+
+var writeFile = async function(filePath, buffer) {
+  if (process.env.vpcStackName) {
+    return writeFileToEFS(filePath, buffer);
+  } else {
+    return writeFileToS3(filePath, buffer);
+  }
+};
+
+var readFile = async function (filePath) {
+  if (process.env.vpcStackName) {
+    return readFileFromEFS(filePath);
+  } else if (cached && process.env.readFromCache) {
+    return readFileFromLocal();
+  } else {
+    return readFileFromS3(filePath);}
+};
+
+var getFileSize = async function (filePath) {
+  if (process.env.vpcStackName) {
+    return (await fs.stat(filePath)).size;
+  } else {
+    const s3Object = await s3
+      .getObject({
+        Bucket: process.env.bucket,
+        Key: filePath,
+      })
+      .promise();
+
+    return Buffer.byteLength(s3Object.Body, "utf8");
+  }
+};
 
 var mean = numbers => (numbers.reduce(sum) / numbers.length)
 
@@ -65,22 +91,42 @@ var readFileFromEFS = async function(filePath){
 var readFileFromS3 = async function (filePath) {
   console.log("Attempting to read file from S3: %s", filePath);
   const readstart = process.hrtime();
-  let fileContents;
   try {
-    fileContents = await s3.getObject({
+    const s3Object = await s3.getObject({
       Bucket: process.env.bucket,
       Key: filePath
     }).promise();
     const readend = process.hrtime(readstart);
     const readTimeMs = readend[0] * 1000 + readend[1] / 1000000;
-    console.log("Retrieved %d bytes from S3", new Blob([fileContents]).size);
     console.log("Read completed in %dms", readTimeMs);
+
+    await fs.writeFile("/tmp/" + fileName, s3Object.Body);
+    cached = true;
     return readTimeMs;
   } catch (error) {
     console.error(error);
     return "Read error: " + error;
   }
 };
+
+var readFileFromLocal = async function () {
+  console.log(
+    "Attempting to read file from local store: %s",
+    "/tmp/" + fileName
+  );
+  const readstart = process.hrtime();
+  let fileContents
+  try {
+    fileContents = await fs.readFile("/tmp/" + fileName, "utf8");
+    const readend = process.hrtime(readstart);
+    const readTimeMs = readend[0] * 1000 + readend[1] / 1000000;
+    console.log("Read completed in %dms", readTimeMs);
+    return readTimeMs;
+  } catch (error) {
+    console.error(error);
+    return "Read error: " + error;
+  }
+}
 
 var writeFileToEFS = async function(filePath, buffer){
   console.log("Attempting to write file to EFS: %s", filePath)
